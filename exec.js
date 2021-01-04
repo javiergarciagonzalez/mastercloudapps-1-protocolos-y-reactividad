db85d8008636be84f37fd0b4caa574af67618686
Javier García González <javier@garciagon.com>
2021-01-04 20:09:27 +0100

    Add exec and install scripts provided by the teacher

---
+++ exec.js
@@ -0,0 +1,19 @@
+const { spawn } = require('child_process');
+function exec(serviceName, command){
+
+    console.log(`Stated service [${serviceName}]`);
+
+    let cmd = spawn(command, [], { cwd: './' + serviceName, shell: true });
+    cmd.stdout.on('data', function(data){
+        process.stdout.write(`[${serviceName}] ${data}`);
+    });
+
+    cmd.stderr.on('data', function(data){
+        process.stderr.write(`[${serviceName}] ${data}`);
+    });
+}
+
+exec('weatherservice', 'node src/server.js');
+exec('toposervice', 'mvn spring-boot:run');
+exec('server','npm start');
+exec('worker','mvn spring-boot:run');
